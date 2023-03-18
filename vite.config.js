bbbb871612b089f8e2ba63897b36386fa8336e9c
evan70 <ivanpaldan@yandex.com>
2023-03-18 02:05:55 +0100

    visualization of build proccess - plugin for vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
     plugins: [
@@ -16,6 +17,14 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }),
+        visualizer({
+            filename: './storage/analyzer/bundle-analyzer.html',
+            open: true,
+            template: 'list', //sunburst, treemap, network, json, list
+            json: false,
+            gzipSize: true,
+            brotliSize: true,
         }),
     ],
 });
