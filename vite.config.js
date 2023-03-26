aa0f78d3ac96dcdbf5c2ec117abf3561c2b7caa5
evan70 <ivanpaldan@yandex.com>
2023-03-26 23:14:21 +0200

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,3 +18,35 @@ export default defineConfig({
     }),
   ],
 })
+
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import vue from '@vitejs/plugin-vue';
+// import { visualizer } from "rollup-plugin-visualizer";
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: 'resources/js/app.js',
+//             ssr: 'resources/js/ssr.js',
+//             refresh: true,
+//         }),
+//         vue({
+//             template: {
+//                 transformAssetUrls: {
+//                     base: null,
+//                     includeAbsolute: false,
+//                 },
+//             },
+//         }),
+//         visualizer({
+//             filename: './storage/analyzer/bundle-analyzer.html',
+//             open: true,
+//             template: 'list', //sunburst, treemap, network, json, list
+//             json: false,
+//             gzipSize: true,
+//             brotliSize: true,
+//         }),
+//     ],
+// });
