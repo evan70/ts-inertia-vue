459d1854e578ba6754ecc753347621d75c3c3a01
evan70 <ivanpaldan@yandex.com>
2023-03-26 23:16:59 +0200

    new file:   bootstrap/ssr/assets/ApplicationLogo-6bd635f3.mjs 	new file:   bootstrap/ssr/assets/AuthenticatedLayout-ff187691.mjs 	new file:   bootstrap/ssr/assets/ConfirmPassword-dd261646.mjs 	new file:   bootstrap/ssr/assets/Dashboard-9eb1d3ea.mjs 	new file:   bootstrap/ssr/assets/DeleteUserForm-584d13ad.mjs 	new file:   bootstrap/ssr/assets/Edit-28422bec.mjs 	new file:   bootstrap/ssr/assets/ForgotPassword-b060bc84.mjs 	new file:   bootstrap/ssr/assets/GuestLayout-54760e75.mjs 	new file:   bootstrap/ssr/assets/Home-996dc823.mjs 	new file:   bootstrap/ssr/assets/Login-af33dc79.mjs 	new file:   bootstrap/ssr/assets/Nav-d977321a.mjs 	new file:   bootstrap/ssr/assets/PrimaryButton-c1bf555c.mjs 	new file:   bootstrap/ssr/assets/Register-56316615.mjs 	new file:   bootstrap/ssr/assets/ResetPassword-8c1e2355.mjs 	new file:   bootstrap/ssr/assets/Settings-e0831d75.mjs 	new file:   bootstrap/ssr/assets/TextInput-fcdf0b16.mjs 	new file:   bootstrap/ssr/assets/UpdatePasswordForm-41cd3ae2.mjs 	new file:   bootstrap/ssr/assets/UpdateProfileInformationForm-9e2da7cf.mjs 	new file:   bootstrap/ssr/assets/Users-66d0aba5.mjs 	new file:   bootstrap/ssr/assets/VerifyEmail-d464ea07.mjs 	new file:   bootstrap/ssr/assets/Welcome-e1076246.mjs 	new file:   bootstrap/ssr/ssr.mjs

---
+++ bootstrap/ssr/assets/Settings-e0831d75.mjs
@@ -0,0 +1,24 @@
+import { ssrRenderAttrs, ssrRenderComponent } from "vue/server-renderer";
+import { _ as _sfc_main$1 } from "./Nav-d977321a.mjs";
+import { useSSRContext } from "vue";
+import "@inertiajs/vue3";
+const _sfc_main = {
+  __name: "Settings",
+  __ssrInlineRender: true,
+  setup(__props) {
+    return (_ctx, _push, _parent, _attrs) => {
+      _push(`<div${ssrRenderAttrs(_attrs)}>`);
+      _push(ssrRenderComponent(_sfc_main$1, null, null, _parent));
+      _push(`<h1 class="text-2xl font-extrabold text-black">Settings</h1></div>`);
+    };
+  }
+};
+const _sfc_setup = _sfc_main.setup;
+_sfc_main.setup = (props, ctx) => {
+  const ssrContext = useSSRContext();
+  (ssrContext.modules || (ssrContext.modules = /* @__PURE__ */ new Set())).add("resources/js/Pages/Settings.vue");
+  return _sfc_setup ? _sfc_setup(props, ctx) : void 0;
+};
+export {
+  _sfc_main as default
+};
