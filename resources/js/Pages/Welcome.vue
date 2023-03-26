374d9048fb52fdca342e38ad3f6cb9fd69a62728
evan70 <ivanpaldan@yandex.com>
2023-03-26 23:15:35 +0200

    new file:   resources/js/Components/README.MD 	new file:   resources/js/Layouts/README.MD 	new file:   resources/js/Pages/README.MD 	modified:   resources/js/Pages/Welcome.vue 	new file:   resources/js/app.ts 	new file:   resources/js/bootstrap.js 	new file:   resources/js/types/globals.d.ts 	modified:   resources/js/types/inertia.d.ts 	new file:   resources/js/types/vite-env.d.ts 	modified:   resources/js/types/vue-shim.d.ts 	new file:   resources/markdown/README.MD 	modified:   resources/views/app.blade.php

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
 import type { PropType } from 'vue'
 import { computed } from 'vue'
-import route from 'ziggy-js'
 import { Head, Link, usePage } from '@inertiajs/vue3'
 
 defineProps({
@@ -11,20 +10,23 @@ defineProps({
   phpVersion: String as PropType<string>,
 })
 
-const user = computed(() => usePage().props?.user)
+const user = computed(() => usePage<{ user: inertia.User }>().props?.user)
 </script>
 
 <template>
-  <Head title="Welcome from js Welcome.vue" />
+  <Head title="Welcome" />
 
   <div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0"
+    class="items-top relative flex min-h-screen justify-center bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0"
   >
-    <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+    <div
+      v-if="canLogin"
+      class="fixed top-0 right-0 hidden px-6 py-4 sm:block"
+    >
       <Link
         v-if="user"
         :href="route('dashboard')"
-        class="text-sm text-gray-700 dark:text-gray-500 underline"
+        class="text-sm text-gray-700 underline dark:text-gray-500"
       >
         Dashboard
       </Link>
@@ -32,7 +34,7 @@ const user = computed(() => usePage().props?.user)
       <template v-else>
         <Link
           :href="route('login')"
-          class="text-sm text-gray-700 dark:text-gray-500 underline"
+          class="text-sm text-gray-700 underline dark:text-gray-500"
         >
           Log in
         </Link>
@@ -40,16 +42,18 @@ const user = computed(() => usePage().props?.user)
         <Link
           v-if="canRegister"
           :href="route('register')"
-          class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline"
+          class="ml-4 text-sm text-gray-700 underline dark:text-gray-500"
         >
           Register
         </Link>
       </template>
     </div>
-    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-      <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+    <div class="mx-auto max-w-6xl sm:px-6 lg:px-8">
+      <div
+        class="mt-4 flex justify-center sm:items-center sm:justify-between"
+      >
         <div
-          class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0"
+          class="ml-4 text-center text-sm text-gray-500 sm:ml-0 sm:text-right"
         >
           Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
         </div>
--- resources/js/types/inertia.d.ts
+++ resources/js/types/inertia.d.ts
@@ -1,19 +1,27 @@
-export {};
+export {}
 declare global {
-  export namespace inertia {
-    export interface Props {
-      user: {
-        id: number;
-        name: string;
-        email: string;
-        created_at: Date;
-        updated_at: Date;
-      };
-      breeze: {
-        [key: string]: boolean;
-      };
-      errorBags: unknown;
-      errors: unknown;
+
+    export namespace inertia {
+
+        export interface User {
+            id: number
+            name: string
+            email: string
+            created_at: Date
+            updated_at: Date
+        }
+
+        export interface Jetstream {
+            [key: string]: boolean
+        }
+
+        export type ErrorBags = undefined | { [key: string]: string[] }
+
+        export type Errors = undefined | string[]
+
+        export type Flash = {
+            message: undefined | string
+            error: undefined | string[]
+        }
     }
-  }
 }
