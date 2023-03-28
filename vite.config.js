32ff223671de96fc6ce15bd5c087341ff584386b
evan70 <ivanpaldan@yandex.com>
2023-03-28 23:29:08 +0200

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
   plugins: [
@@ -16,37 +17,14 @@ export default defineConfig({
         },
       },
     }),
+    visualizer({
+        filename: './storage/bundle-analyzer.html',
+        open: false,
+        template: 'network', //sunburst, treemap, network, json, list
+        //json: false,
+        gzipSize: true,
+        brotliSize: true,
+    }),
   ],
 })
 
-
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-// import vue from '@vitejs/plugin-vue';
-// import { visualizer } from "rollup-plugin-visualizer";
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: 'resources/js/app.js',
-//             ssr: 'resources/js/ssr.js',
-//             refresh: true,
-//         }),
-//         vue({
-//             template: {
-//                 transformAssetUrls: {
-//                     base: null,
-//                     includeAbsolute: false,
-//                 },
-//             },
-//         }),
-//         visualizer({
-//             filename: './storage/analyzer/bundle-analyzer.html',
-//             open: true,
-//             template: 'list', //sunburst, treemap, network, json, list
-//             json: false,
-//             gzipSize: true,
-//             brotliSize: true,
-//         }),
-//     ],
-// });
