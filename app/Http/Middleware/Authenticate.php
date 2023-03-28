a3ccec1c845a340c69a3c98b72990e332b6d93bd
evan70 <ivanpaldan@yandex.com>
2023-03-28 23:33:39 +0200

    modified:   app/Console/Kernel.php 	modified:   app/Exceptions/Handler.php 	modified:   app/Http/Controllers/Controller.php 	modified:   app/Http/Kernel.php 	modified:   app/Http/Middleware/Authenticate.php 	modified:   app/Http/Middleware/HandleInertiaRequests.php 	modified:   app/Http/Middleware/RedirectIfAuthenticated.php 	modified:   app/Models/User.php

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
@@ -11,10 +12,8 @@ class Authenticate extends Middleware
      *
      * @return string|null
      */
-    protected function redirectTo($request)
+    protected function redirectTo(Request $request): ?string
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        return $request->expectsJson() ? null : route('login');
     }
 }
